3ce5920e40bcadc4e02948baff74f65dd5f2604e
Bohdan Sosunovych <bohdan.sosunovych@gmail.com>
2024-01-11 13:17:58 +0200

    added_book-dto_and_mapping

--- src/main/java/book/store/BookStoreApplication.java
+++ src/main/java/book/store/BookStoreApplication.java
@@ -1,34 +1,12 @@
 package book.store;
 
-import book.store.model.Book;
-import book.store.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
-    @Autowired
-    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book kobzar = new Book();
-            kobzar.setTitle("Kobzar");
-            kobzar.setAuthor("Taras Shevchenko");
-            kobzar.setIsbn("1Isbn");
-            kobzar.setPrice(BigDecimal.valueOf(99.99));
-
-            bookService.save(kobzar);
-            System.out.println(bookService.findAll());
-        };
     }
 }
--- src/main/java/book/store/service/impl/BookServiceImpl.java
+++ src/main/java/book/store/service/impl/BookServiceImpl.java
@@ -1,28 +1,41 @@
 package book.store.service.impl;
 
+import book.store.dto.BookDto;
+import book.store.dto.CreateBookRequestDto;
+import book.store.exception.EntityNotFoundException;
+import book.store.mapper.BookMapper;
 import book.store.model.Book;
 import book.store.repository.BookRepository;
 import book.store.service.BookService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Override
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        Book savedBook = bookRepository.save(book);
+        return bookMapper.toDto(savedBook);
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book with id: " + id)
+        );
+        return bookMapper.toDto(book);
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
