849359cca92ddf412e4cbfc321e587d576b63954
Bohdan Sosunovych <bohdan.sosunovych@gmail.com>
2024-01-16 20:45:18 +0200

    corrected_solution

--- src/main/java/book/store/controller/BookController.java
+++ src/main/java/book/store/controller/BookController.java
@@ -5,8 +5,16 @@ import book.store.dto.CreateBookRequestDto;
 import book.store.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
