f0a7400d939e9d6c93eef962966eaa2eed1fb8cf
Bohdan Sosunovych <bohdan.sosunovych@gmail.com>
2024-02-15 21:52:29 +0200

    improved_solution

--- src/test/java/book/store/config/EnvConfig.java
+++
@@ -1,9 +0,0 @@
-package book.store.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("file:.env")
-public class EnvConfig {
-}
